4ba013f4439073fa5cdd4cb69786cba37c6804ad
eromose-le <erons24@yahoo.com>
2021-11-15 19:38:29 +0100

    ...

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URL);
+  const conn = await mongoose.connect(
+    'mongodb+srv://tee:tee123@baconsulting.wmymi.mongodb.net/investment-api?retryWrites=true&w=majority'
+  );
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
